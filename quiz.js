032de7eadf0f18235ebc32f4f4a4ed6aea1c3d90
Christian Hidalgo <indianajonez@Christians-MacBook-Pro.local>
2023-12-12 14:31:42 -0500

    9112Project

---
+++ quiz.js
@@ -0,0 +1,10 @@
+currentDay = 12; // or put current day
+daysTillChrist = 25;
+
+let getDaysUntilChristmas = function (daysTillChrist, currentDay) {
+  let sum = daysTillChrist - currentDay;
+  return sum;
+};
+
+let result = getDaysUntilChristmas(daysTillChrist, currentDay);
+console.log(result);
